7109f0524d773be58dc7d2cf9d07db1a5d108bfd
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-28 23:34:09 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "load_balancer_dns" {
+  value = aws_lb.alb.dns_name
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.mysql1.endpoint
+}
+
+output "asg_name" {
+  value = aws_autoscaling_group.asg.name
+}
