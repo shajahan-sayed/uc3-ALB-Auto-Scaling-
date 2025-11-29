3a993e22fbd966449600166fd166747cac8e0899
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-29 16:51:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -281,9 +281,11 @@ resource "aws_db_instance" "mysql2" {
    username                = var.db_username
    password                = var.db_password
    multi_az                = true
-   skip_final_snapshot = true
+   
    vpc_security_group_ids  = [aws_security_group.db_sg.id]
    db_subnet_group_name    = aws_db_subnet_group.mysql2.name
+
+  skip_final_snapshot = true 
 }
      
     
